7e8876c2793e13af97191be58fdbc1814ed7c475
Dan Keech <ddkeech@gmail.com>
2020-08-19 18:41:18 -0400

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_keechd',
-  password        : 'helpersecretary',
+  password        : ''
   database        : 'cs340_keechd'
 });
 module.exports.pool = pool;
